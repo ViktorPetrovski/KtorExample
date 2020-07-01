a437da48391382b75e78fbd1b41a11d42e315900
Viktor <Viktor@imgur.com>
2020-07-01 11:35:18 +0200

    Update gson with moshi

--- src/Application.kt
+++ src/Application.kt
@@ -1,21 +1,25 @@
 package doc.ktor
 
+import com.ryanharter.ktor.moshi.moshi
 import database.DatabaseFactory
-import features
 import post.route.post
 import io.ktor.application.Application
 import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
 import io.ktor.routing.routing
-import io.netty.handler.codec.DefaultHeaders
 import post.data.PostsRepositoryImpl
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module(testing: Boolean = false) {
     DatabaseFactory.init()
+    configureFeatures()
     routing {
         post(PostsRepositoryImpl())
-        features()
     }
+}
+
+fun Application.configureFeatures() {
+    install(ContentNegotiation) { moshi() }
     install(io.ktor.features.DefaultHeaders)
 }
